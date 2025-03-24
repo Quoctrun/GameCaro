a227baf658808bac1f229be8e6e68e3754e97767
Phan Thanh Dat <2251120007@ut.edu.vn>
2025-03-25 02:58:16 +0700

    create branch #7

--- gameCaro/gameCaro/Form1.Designer.cs
+++ gameCaro/gameCaro/Form1.Designer.cs
@@ -11,7 +11,6 @@
         private System.Windows.Forms.Panel panelChat;
         private System.Windows.Forms.TextBox textbox_IP;
         private System.Windows.Forms.Button BtnLAN;
-        private System.Windows.Forms.Button btnReset;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -29,34 +28,42 @@
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox_infoplayer = new System.Windows.Forms.PictureBox();
             this.pnlChessBoard = new System.Windows.Forms.Panel();
             this.panelInfo = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelChat = new System.Windows.Forms.Panel();
             this.progressBar_cooldown = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox_playername = new System.Windows.Forms.TextBox();
-            this.btnReset = new System.Windows.Forms.Button();
             this.BtnLAN = new System.Windows.Forms.Button();
             this.textbox_IP = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.tmcooldown = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_infoplayer)).BeginInit();
             this.panelInfo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelChat.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox_infoplayer
             // 
             this.pictureBox_infoplayer.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.pictureBox_infoplayer.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox_infoplayer.Location = new System.Drawing.Point(0, 0);
             this.pictureBox_infoplayer.Name = "pictureBox_infoplayer";
-            this.pictureBox_infoplayer.Size = new System.Drawing.Size(187, 169);
+            this.pictureBox_infoplayer.Size = new System.Drawing.Size(193, 175);
             this.pictureBox_infoplayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_infoplayer.TabIndex = 6;
             this.pictureBox_infoplayer.TabStop = false;
+            this.pictureBox_infoplayer.Click += new System.EventHandler(this.pictureBox_infoplayer_Click);
             // 
             // pnlChessBoard
             // 
@@ -64,9 +71,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlChessBoard.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.pnlChessBoard.BackColor = System.Drawing.Color.White;
-            this.pnlChessBoard.Location = new System.Drawing.Point(12, 12);
+            this.pnlChessBoard.Location = new System.Drawing.Point(0, 27);
             this.pnlChessBoard.Name = "pnlChessBoard";
-            this.pnlChessBoard.Size = new System.Drawing.Size(991, 657);
+            this.pnlChessBoard.Size = new System.Drawing.Size(979, 657);
             this.pnlChessBoard.TabIndex = 0;
             this.pnlChessBoard.TabStop = true;
             // 
@@ -74,7 +81,7 @@
             // 
             this.panelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panelInfo.Controls.Add(this.pictureBox_infoplayer);
-            this.panelInfo.Location = new System.Drawing.Point(1009, 199);
+            this.panelInfo.Location = new System.Drawing.Point(32, 454);
             this.panelInfo.Name = "panelInfo";
             this.panelInfo.Size = new System.Drawing.Size(193, 175);
             this.panelInfo.TabIndex = 1;
@@ -84,40 +91,53 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox1.Image = global::gameCaro.Properties.Resources.thi_dau_co_caro;
-            this.pictureBox1.Location = new System.Drawing.Point(0, -3);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(190, 178);
+            this.pictureBox1.Size = new System.Drawing.Size(193, 178);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // panelChat
             // 
             this.panelChat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panelChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.panelChat.Controls.Add(this.progressBar_cooldown);
+            this.panelChat.Controls.Add(this.panel1);
             this.panelChat.Controls.Add(this.label1);
             this.panelChat.Controls.Add(this.textBox_playername);
-            this.panelChat.Controls.Add(this.btnReset);
+            this.panelChat.Controls.Add(this.panelInfo);
             this.panelChat.Controls.Add(this.BtnLAN);
             this.panelChat.Controls.Add(this.textbox_IP);
-            this.panelChat.Location = new System.Drawing.Point(1208, 15);
+            this.panelChat.Location = new System.Drawing.Point(976, 27);
             this.panelChat.Name = "panelChat";
-            this.panelChat.Size = new System.Drawing.Size(230, 654);
+            this.panelChat.Size = new System.Drawing.Size(255, 657);
             this.panelChat.TabIndex = 2;
+            this.panelChat.Paint += new System.Windows.Forms.PaintEventHandler(this.panelChat_Paint);
             // 
             // progressBar_cooldown
             // 
-            this.progressBar_cooldown.Location = new System.Drawing.Point(14, 58);
+            this.progressBar_cooldown.Location = new System.Drawing.Point(32, 81);
             this.progressBar_cooldown.Name = "progressBar_cooldown";
-            this.progressBar_cooldown.Size = new System.Drawing.Size(194, 23);
+            this.progressBar_cooldown.Size = new System.Drawing.Size(193, 24);
             this.progressBar_cooldown.TabIndex = 9;
+            this.progressBar_cooldown.Click += new System.EventHandler(this.progressBar_cooldown_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(32, 251);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 178);
+            this.panel1.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Location = new System.Drawing.Point(26, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(203, 33);
             this.label1.TabIndex = 7;
@@ -127,28 +147,20 @@
             // textBox_playername
             // 
             this.textBox_playername.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_playername.Location = new System.Drawing.Point(14, 102);
+            this.textBox_playername.Location = new System.Drawing.Point(32, 121);
             this.textBox_playername.Name = "textBox_playername";
             this.textBox_playername.ReadOnly = true;
-            this.textBox_playername.Size = new System.Drawing.Size(197, 22);
+            this.textBox_playername.Size = new System.Drawing.Size(193, 22);
             this.textBox_playername.TabIndex = 8;
             this.textBox_playername.Text = "player name";
             this.textBox_playername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // btnReset
-            // 
-            this.btnReset.Location = new System.Drawing.Point(14, 233);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(200, 30);
-            this.btnReset.TabIndex = 5;
-            this.btnReset.Text = "Reset Game";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.textBox_playername.TextChanged += new System.EventHandler(this.textBox_playername_TextChanged);
             // 
             // BtnLAN
             // 
-            this.BtnLAN.Location = new System.Drawing.Point(14, 187);
+            this.BtnLAN.Location = new System.Drawing.Point(32, 196);
             this.BtnLAN.Name = "BtnLAN";
-            this.BtnLAN.Size = new System.Drawing.Size(200, 23);
+            this.BtnLAN.Size = new System.Drawing.Size(193, 29);
             this.BtnLAN.TabIndex = 4;
             this.BtnLAN.Text = "LAN Connect";
             this.BtnLAN.UseVisualStyleBackColor = true;
@@ -156,39 +168,84 @@
             // textbox_IP
             // 
             this.textbox_IP.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textbox_IP.Location = new System.Drawing.Point(14, 143);
+            this.textbox_IP.Location = new System.Drawing.Point(32, 158);
             this.textbox_IP.Name = "textbox_IP";
-            this.textbox_IP.Size = new System.Drawing.Size(200, 22);
+            this.textbox_IP.Size = new System.Drawing.Size(193, 22);
             this.textbox_IP.TabIndex = 3;
             this.textbox_IP.Text = "IP";
             this.textbox_IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textbox_IP.TextChanged += new System.EventHandler(this.textbox_IP_TextChanged);
             // 
-            // panel1
+            // tmcooldown
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(1009, 15);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(193, 178);
-            this.panel1.TabIndex = 7;
+            this.tmcooldown.Tick += new System.EventHandler(this.tmcooldown_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1231, 28);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newGameToolStripMenuItem.Text = "New game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1433, 679);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pnlChessBoard);
-            this.Controls.Add(this.panelInfo);
+            this.ClientSize = new System.Drawing.Size(1231, 680);
             this.Controls.Add(this.panelChat);
+            this.Controls.Add(this.pnlChessBoard);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Game Caro";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_infoplayer)).EndInit();
             this.panelInfo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelChat.ResumeLayout(false);
             this.panelChat.PerformLayout();
             this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -200,5 +257,11 @@
         private System.Windows.Forms.ProgressBar progressBar_cooldown;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox_infoplayer;
+        private System.Windows.Forms.Timer tmcooldown;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
     }
 }
--- gameCaro/gameCaro/Form1.cs
+++ gameCaro/gameCaro/Form1.cs
@@ -21,12 +21,122 @@ namespace gameCaro
             InitializeComponent();// Khởi tạo các thành phần giao diện của Form
 
             ChessBoard = new ChessBoardManager(pnlChessBoard, textBox_playername, pictureBox_infoplayer);// Khởi tạo đối tượng quản lý bàn cờ với Panelchess,playername,picture_info
+            ChessBoard.EndedGame += ChessBoard_EndedGame;// Gán sự kiện kết thúc game cho bàn cờ
+            ChessBoard.PlayerMarked += ChessBoard_PlayerMarked;// Gán sự kiện đánh cờ cho bàn cờ 
 
+
+            progressBar_cooldown.Step = Cons.COOL_DOWN_STEP;// Đặt giá trị bước nhảy của progressbar
+            progressBar_cooldown.Maximum = Cons.COOL_DOWN_TIME;// Đặt giá trị lớn nhất của progressbar
+            progressBar_cooldown.Value = 0;// Đặt giá trị ban đầu của progressbar
+            
+            tmcooldown.Interval = Cons.COOL_DOWN_INTERVAL;// Đặt thời gian cập nhật của progressbar
 
             ChessBoard.DrawChessBoard();// Vẽ bàn cờ khi Form được khởi tạo
+        
+            
+
         }
-   
+
+        #region
+        void EndGame()
+        {
+            tmcooldown.Stop();// Dừng đếm thời gian
+            pnlChessBoard.Enabled = false;// Không cho người chơi đánh tiếp
+            resetToolStripMenuItem.Enabled = false;// Không cho phép quay lại
+            MessageBox.Show("Kết thúc");
+        }
+
+        void NewGame()
+        {
+            progressBar_cooldown.Value = 0;// Đặt giá trị ban đầu của progressbar
+            tmcooldown.Stop();// Dừng đếm thời gian
+            resetToolStripMenuItem.Enabled = true;// Cho phép quay lại
+            ChessBoard.DrawChessBoard();// Vẽ lại bàn cờ
+        }
+
+        void Quit()
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thông báo", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+                Application.Exit();// Thoát ứng dụng           
+        }
+
+        void Reset()
+        {
+            ChessBoard.Reset();// Đặt lại bàn cờ
+        }
+        private void ChessBoard_PlayerMarked(object sender, EventArgs e)
+        {
+            tmcooldown.Start();// Bắt đầu đếm thời gian
+            progressBar_cooldown.Value = 0;// Đặt giá trị ban đầu của progressbar
+        }
+
+        private void ChessBoard_EndedGame(object sender, EventArgs e)
+        {
+            EndGame();
+        }
+                  
+        private void progressBar_cooldown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmcooldown_Tick(object sender, EventArgs e)
+        {
+            progressBar_cooldown.PerformStep();// Cập nhật giá trị của progressbar
+            
+            if (progressBar_cooldown.Value >= progressBar_cooldown.Maximum)
+            {
+                
+                EndGame();// Kết thúc game
+            }
+        }
+
+        private void panelChat_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox_infoplayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textbox_IP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_playername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
 
 
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+      
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Quit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thông báo", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.Cancel)
+                      e.Cancel = true;
+        }
+
+        #endregion
     }
 }
