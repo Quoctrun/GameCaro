24734a9bd3b6b5107cf233addbc6dcff5251b4f8
teohoan <hoannc0185@ut.edu.vn>
2025-03-30 19:23:28 +0700

    create branch #9

--- gameCaro/gameCaro/Form1.Designer.cs
+++ gameCaro/gameCaro/Form1.Designer.cs
@@ -1,45 +1,48 @@
 ﻿namespace gameCaro
 {
-    partial class Form1
+partial class Form1
+{
+/// <summary>
+/// Required designer variable.
+/// </summary>
+private System.ComponentModel.IContainer components = null;
+private System.Windows.Forms.Panel pnlChessBoard;
+private System.Windows.Forms.Panel panelInfo;
+private System.Windows.Forms.Panel panelChat;
+private System.Windows.Forms.TextBox textbox_IP;
+private System.Windows.Forms.Button BtnLAN;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Panel pnlChessBoard;
-        private System.Windows.Forms.Panel panelInfo;
-        private System.Windows.Forms.Panel panelChat;
-        private System.Windows.Forms.TextBox textbox_IP;
-        private System.Windows.Forms.Button BtnLAN;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        protected override void Dispose(bool disposing)
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
+        base.Dispose(disposing);
+    }
 
-        #region Windows Form Designer generated code
+    #region Windows Form Designer generated code
 
-        private void InitializeComponent()
-        {
+    private void InitializeComponent()
+    {
             this.components = new System.ComponentModel.Container();
             this.pictureBox_infoplayer = new System.Windows.Forms.PictureBox();
             this.pnlChessBoard = new System.Windows.Forms.Panel();
             this.panelInfo = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelChat = new System.Windows.Forms.Panel();
+            this.rtbChatBox = new System.Windows.Forms.RichTextBox();
             this.progressBar_cooldown = new System.Windows.Forms.ProgressBar();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox_playername = new System.Windows.Forms.TextBox();
             this.BtnLAN = new System.Windows.Forms.Button();
             this.textbox_IP = new System.Windows.Forms.TextBox();
+            this.txtChatInput = new System.Windows.Forms.TextBox();
+            this.btnSendChat = new System.Windows.Forms.Button();
             this.tmcooldown = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -59,7 +62,7 @@
             this.pictureBox_infoplayer.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox_infoplayer.Location = new System.Drawing.Point(0, 0);
             this.pictureBox_infoplayer.Name = "pictureBox_infoplayer";
-            this.pictureBox_infoplayer.Size = new System.Drawing.Size(193, 175);
+            this.pictureBox_infoplayer.Size = new System.Drawing.Size(193, 46);
             this.pictureBox_infoplayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_infoplayer.TabIndex = 6;
             this.pictureBox_infoplayer.TabStop = false;
@@ -81,9 +84,9 @@
             // 
             this.panelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panelInfo.Controls.Add(this.pictureBox_infoplayer);
-            this.panelInfo.Location = new System.Drawing.Point(32, 454);
+            this.panelInfo.Location = new System.Drawing.Point(32, 285);
             this.panelInfo.Name = "panelInfo";
-            this.panelInfo.Size = new System.Drawing.Size(193, 175);
+            this.panelInfo.Size = new System.Drawing.Size(193, 47);
             this.panelInfo.TabIndex = 1;
             // 
             // pictureBox1
@@ -93,7 +96,7 @@
             this.pictureBox1.Image = global::gameCaro.Properties.Resources.thi_dau_co_caro;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(193, 178);
+            this.pictureBox1.Size = new System.Drawing.Size(193, 74);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -103,6 +106,7 @@
             // 
             this.panelChat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panelChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelChat.Controls.Add(this.rtbChatBox);
             this.panelChat.Controls.Add(this.progressBar_cooldown);
             this.panelChat.Controls.Add(this.panel1);
             this.panelChat.Controls.Add(this.label1);
@@ -110,15 +114,26 @@
             this.panelChat.Controls.Add(this.panelInfo);
             this.panelChat.Controls.Add(this.BtnLAN);
             this.panelChat.Controls.Add(this.textbox_IP);
+            this.panelChat.Controls.Add(this.txtChatInput);
+            this.panelChat.Controls.Add(this.btnSendChat);
             this.panelChat.Location = new System.Drawing.Point(976, 27);
             this.panelChat.Name = "panelChat";
             this.panelChat.Size = new System.Drawing.Size(255, 657);
             this.panelChat.TabIndex = 2;
             this.panelChat.Paint += new System.Windows.Forms.PaintEventHandler(this.panelChat_Paint);
             // 
+            // rtbChatBox
+            // 
+            this.rtbChatBox.Location = new System.Drawing.Point(31, 353);
+            this.rtbChatBox.Name = "rtbChatBox";
+            this.rtbChatBox.Size = new System.Drawing.Size(194, 240);
+            this.rtbChatBox.TabIndex = 13;
+            this.rtbChatBox.Text = "";
+            this.rtbChatBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
             // progressBar_cooldown
             // 
-            this.progressBar_cooldown.Location = new System.Drawing.Point(32, 81);
+            this.progressBar_cooldown.Location = new System.Drawing.Point(31, 50);
             this.progressBar_cooldown.Name = "progressBar_cooldown";
             this.progressBar_cooldown.Size = new System.Drawing.Size(193, 24);
             this.progressBar_cooldown.TabIndex = 9;
@@ -128,16 +143,16 @@
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(32, 251);
+            this.panel1.Location = new System.Drawing.Point(32, 204);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(193, 178);
+            this.panel1.Size = new System.Drawing.Size(193, 75);
             this.panel1.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Location = new System.Drawing.Point(26, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(203, 33);
             this.label1.TabIndex = 7;
@@ -147,7 +162,7 @@
             // textBox_playername
             // 
             this.textBox_playername.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_playername.Location = new System.Drawing.Point(32, 121);
+            this.textBox_playername.Location = new System.Drawing.Point(31, 89);
             this.textBox_playername.Name = "textBox_playername";
             this.textBox_playername.ReadOnly = true;
             this.textBox_playername.Size = new System.Drawing.Size(193, 22);
@@ -158,7 +173,7 @@
             // 
             // BtnLAN
             // 
-            this.BtnLAN.Location = new System.Drawing.Point(32, 196);
+            this.BtnLAN.Location = new System.Drawing.Point(31, 169);
             this.BtnLAN.Name = "BtnLAN";
             this.BtnLAN.Size = new System.Drawing.Size(193, 29);
             this.BtnLAN.TabIndex = 4;
@@ -169,13 +184,31 @@
             // textbox_IP
             // 
             this.textbox_IP.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textbox_IP.Location = new System.Drawing.Point(32, 158);
+            this.textbox_IP.Location = new System.Drawing.Point(31, 129);
             this.textbox_IP.Name = "textbox_IP";
             this.textbox_IP.Size = new System.Drawing.Size(193, 22);
             this.textbox_IP.TabIndex = 3;
             this.textbox_IP.Text = "IP";
             this.textbox_IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textbox_IP.TextChanged += new System.EventHandler(this.textbox_IP_TextChanged);
+            // 
+            // txtChatInput
+            // 
+            this.txtChatInput.Location = new System.Drawing.Point(31, 599);
+            this.txtChatInput.Name = "txtChatInput";
+            this.txtChatInput.Size = new System.Drawing.Size(138, 22);
+            this.txtChatInput.TabIndex = 11;
+            this.txtChatInput.TextChanged += new System.EventHandler(this.txtChatInput_TextChanged);
+            // 
+            // btnSendChat
+            // 
+            this.btnSendChat.Location = new System.Drawing.Point(178, 598);
+            this.btnSendChat.Name = "btnSendChat";
+            this.btnSendChat.Size = new System.Drawing.Size(47, 23);
+            this.btnSendChat.TabIndex = 12;
+            this.btnSendChat.Text = "Send";
+            this.btnSendChat.UseVisualStyleBackColor = true;
+            this.btnSendChat.Click += new System.EventHandler(this.btnSendChat_Click);
             // 
             // tmcooldown
             // 
@@ -249,21 +282,24 @@
             this.ResumeLayout(false);
             this.PerformLayout();
 
-        }
+    }
 
-        #endregion
+    #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox_playername;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ProgressBar progressBar_cooldown;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox_infoplayer;
-        private System.Windows.Forms.Timer tmcooldown;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.TextBox textBox_playername;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ProgressBar progressBar_cooldown;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PictureBox pictureBox_infoplayer;
+    private System.Windows.Forms.Timer tmcooldown;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button btnSendChat;
+        private System.Windows.Forms.TextBox txtChatInput;
+        private System.Windows.Forms.RichTextBox rtbChatBox;
     }
 }
